e31955156527a3591d08ada926eb7748e7aa7f3c
Alan del Fresno <alandelfresno2@gmail.com>
2023-09-30 12:54:13 -0300

    Background and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from 'src/components/header/header.component';
+import { FooterButtonComponent } from 'src/components/footer-button/footer-button.component';
+import { ContentComponent } from 'src/components/content/content.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { IonicModule } from '@ionic/angular';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot()
+    IonicModule.forRoot(),
+    HeaderComponent,
+    FooterButtonComponent,
+    ContentComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/footer-button/footer-button.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-footer-button',
+  templateUrl: './footer-button.component.html',
+  styleUrls: ['./footer-button.component.scss']
+})
+export class FooterButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
